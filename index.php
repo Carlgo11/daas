96a6f9ab0edf81b3f39934967eface0fed352444
Edwin <git@olback.net>
2017-03-02 19:57:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ $_SESSION['dox'] = htmlspecialchars($_GET["q"]);
     <link rel="icon" href="res/icon.jpg">
 
     <!-- Title -->
-    <title>Simple Doxer</title>
+    <title>Doxing as a Service (DAAS)</title>
 
     <!-- Bootstrap CSS + Font-Awesome Icon Lib + Tether.io -->
     <link href="res/bootstrap.min.css" rel="stylesheet">
@@ -42,7 +42,7 @@ $_SESSION['dox'] = htmlspecialchars($_GET["q"]);
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#mainNav" aria-controls="#mainNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <a class="navbar-brand" href="./">Simple Doxing Tool</a>
+      <a class="navbar-brand" href="./">Doxing as a Service (DAAS)</a>
 
       <div class="collapse navbar-collapse" id="mainNav">
         <ul class="navbar-nav mr-auto">
